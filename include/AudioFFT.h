1f007561fbf2569d4b44fb65f418b762538862e3
gu-on <kevin@gu-on.com>
2025-01-18 23:32:19 +0000

    Remove unused typedef

--- include/AudioFFT.h
+++ include/AudioFFT.h
@@ -156,13 +156,6 @@ namespace audiofft
     std::unique_ptr<detail::AudioFFTImpl> _impl;
   };
 
-
-  /**
-   * @deprecated
-   * @brief Let's keep an AudioFFTBase type around for now because it has been here already in the 1st version in order to avoid breaking existing code.
-   */
-  typedef AudioFFT AudioFFTBase;
-
 } // End of namespace
 
 #endif // Header guard
